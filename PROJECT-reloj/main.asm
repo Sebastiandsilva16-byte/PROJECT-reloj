ed90c19ce327aa782ccaf1ea6c36faab4721be44
Sebastiandsilva16-byte <sebastiandsilva16@gmail.com>
2026-02-20 19:45:28 -0600

    FUNCIONA PERO
    fuciona lo de que actualize los bits indivudualmente pero hay mucho texto voy a intentar optimizarlo

--- PROJECT-reloj/main.asm
+++ PROJECT-reloj/main.asm
@@ -68,11 +68,12 @@ SETUP:
 
     // Inicializar registros
     CLR R16		// Multifuncional
-    CLR R17		// Contador de ciclos timer (para 1 segundo)
-    CLR R18		// Cuenta (S)
+    CLR R17		// Contador de ciclos timer 10ms
+    CLR R18		// Cuenta (S unidades)
 	CLR R19		// (se usa para buscar los valores R18) busca los valores para el display
 	CLR R20		// Se usa Zhigh eb DIPLAY
     CLR R21		// Seleciona Display
+	CLR R22		// Cuenta Decenas
 
 	CLR R25		// Agarra los valores de R19 Para Actualizar el display
 	CLR R26		// Cuenta para las decenas de segundos (S)
@@ -82,16 +83,16 @@ SETUP:
 
 MAIN_LOOP:
     CALL TIEMPO
-	CALL DISPLAYSEL
-	CALL DISPLAY		//Actualiza el display actual
-    RJMP MAIN_LOOP
-
-DISPLAYSEL:
-   
-    CPI R21, 4       // Verifica si R21 es 4 o mayor
+	INC R21 //Va haciendo na cuenta para actulizar los contadores individualmente
+    CPI R21, 2     
     BRLO DISPSEL     // Si R21 < 4, está bien
     CLR R21          // Si R21 >= 4, resetea a 0
 DISPSEL:
+	CALL DISPLAYSEL
+    RJMP MAIN_LOOP
+
+DISPLAYSEL:
+
     CPI R21, 0 // Compara con 0
     BREQ DISP0 // Salta a DISP0 si R21 = 0
     CPI R21, 1 // Compara con 1
@@ -107,6 +108,8 @@ DISP0:
     CBI PORTD, 5    // Apaga PD5
     CBI PORTD, 6    // Apaga PD6
     SBI PORTD, 3    // Enciende PD3 (DISP0)
+	CALL DISPLAY0
+
     RJMP FINDISP
 
 DISP1:
@@ -114,6 +117,7 @@ DISP1:
     CBI PORTD, 5    // Apaga PD5
     CBI PORTD, 6    // Apaga PD6
     SBI PORTD, 4    // Enciende PD4 (DISP1)
+	CALL DISPLAY1
     RJMP FINDISP
 
 DISP2:
@@ -133,9 +137,7 @@ DISP3:
 FINDISP:
     RET
 
-
-
-DISPLAY:
+DISPLAY0:
 	PUSH ZH
     PUSH ZL
 
@@ -161,16 +163,70 @@ DISPLAY:
     SBRS R25, 6
     CBI PORTD, PORTD2
 
+	//limpia para que no afecte los otros diplays-----------------------------
+	CLR R25
+	OUT PORTC, R25  
+	//Actualiza el ultimo segmento
+	SBRC R25, 6
+    SBI PORTD, PORTD2
+    SBRS R25, 6
+    CBI PORTD, PORTD2 //----------------------
+
+RET
+
+DISPLAY1:
+	PUSH ZH
+    PUSH ZL
+
+    LDI ZH, HIGH(disp7seg << 1)
+    LDI ZL, LOW(disp7seg << 1)
+
+	// busca los valores para el 7 segmentos
+	MOV R19, R22
+	lsl r19		//mueve los bits a la iz (multiplica por 2)
+	ADD ZL, R19
+	LDI R20, 0
+	ADC ZH, R20
+	LPM R19, Z
+	POP ZL
+    POP ZH
+
+	//Actualiza todo el portC (6 segmentos)
+	MOV R25, R19
+	OUT PORTC, R25  
+	//Actualiza el ultimo segmento
+	SBRC R25, 6
+    SBI PORTD, PORTD2
+    SBRS R25, 6
+    CBI PORTD, PORTD2
+
+	//limpia para que no afecte los otros diplays //----------------------
+	CLR R25
+	OUT PORTC, R25  
+	//Actualiza el ultimo segmento 
+	SBRC R25, 6
+    SBI PORTD, PORTD2
+    SBRS R25, 6
+    CBI PORTD, PORTD2 //----------------------
+
+
+RET
 
 TIEMPO:
+
     CPI R17, 100
-    BRLO TIMER_RET
+    BRLO TIMER_RET //Cuenta los 10ms
     CLR R17
-    INC R18
-	INC R21 //diplay sel
+
+    INC R18			//Unidades de Segundos
     CPI R18, 10
     BRLO TIMER_RET
 	CLR R18
+
+	INC R22			//Decenas de segundos
+	CPI R22, 6
+	BRLO TIMER_RET
+	CLR R22
 
 TIMER_RET:
     RET
